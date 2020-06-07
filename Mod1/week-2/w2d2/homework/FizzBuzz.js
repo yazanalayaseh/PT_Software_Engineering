5e447c217b24238b01992951718b635768aa8244
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-06-06 21:25:24 -0400

    Fizz Buzz answered

---
+++ Mod1/week-2/w2d2/homework/FizzBuzz.js
@@ -0,0 +1,10 @@
+for(i = 1; i<100; i++){
+    if(i % 3 === 0){
+        console.log("Fizz");
+    }
+    if(i % 5 === 0){
+        console.log("Buzz");
+    }
+    else(i % 3 === 0 &&  (i % 5 ===0))
+        console.log("FizzBuzz");
+}
