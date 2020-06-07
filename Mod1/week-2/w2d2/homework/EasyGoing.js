924786b5f5941fbea272a5214a2915d09d139bc6
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-06-06 21:23:34 -0400

    Easy Going answered

--- Mod1/week-2/w2d2/homework/EasyGoing.js
+++ Mod1/week-2/w2d2/homework/EasyGoing.js
@@ -1,22 +1,3 @@
 for(i =1; i<=20; i++){
     console.log(i)
-}
-
-
-for(i = 0; i<=200; i++){
-    if (i === 0){
-        console.log(i+ "is even" );
-    }
-else if (i % 2 === 0){
-    console.log(i + "is even ");
-
-    }
-    else{
-        console.log(i + "is odd");
-    }
-}
-
-
-for(i=0; i<200; i=i+2){
-    console.log(i+2)
-}
+};
