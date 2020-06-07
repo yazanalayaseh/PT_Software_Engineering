3c87f0dca1d50f48687491dc68581c75a04d09a9
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-06-07 00:32:58 -0400

    NinjaTurtles answered

---
+++ Mod1/week-2/w2d2/homework/NinjaTurtles.js
@@ -0,0 +1,17 @@
+//For loop method 
+
+/*const NinjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+for(let i = 0; i<NinjaTurtles.length; i++){
+    NinjaTurtles[i] = NinjaTurtles[i] .toUpperCase();
+    console.log(NinjaTurtles[i]);
+}*/
+
+//Another method for of loop 
+
+const NinjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+for (let NinjaTurtle of NinjaTurtles){
+    console.log(NinjaTurtle.toUpperCase());
+}
+
+
+
