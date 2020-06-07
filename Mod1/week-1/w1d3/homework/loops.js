6ddead105565b572f7071aa195745b88f50275bd
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-06-07 15:12:42 -0400

    Finished loop practice

---
+++ Mod1/week-1/w1d3/homework/loops.js
@@ -0,0 +1,25 @@
+// Even number ascending 
+for (i = 0; i<=98; i= i+2){
+    console.log(i)
+}
+// Even number descending 
+for (i = 98; i>=0; i=i-2){
+    console.log(i)
+}
+
+// ODD numbers ascending 
+
+for (i = 0; i<=99; i++){
+    if(i%2===1){
+        console.log(i)
+    }
+}
+
+// loops the log numbers descending
+for (i=99; i<0; i++){
+    console.log(i)
+}
+
+for(i=3; i<90; i=i*3){
+    console.log(i);
+}
