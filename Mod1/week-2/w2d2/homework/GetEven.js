3d237cf1181486a26d9582f5c23fd353ad2d54f0
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-06-06 21:24:34 -0400

    Get Even answered

---
+++ Mod1/week-2/w2d2/homework/GetEven.js
@@ -0,0 +1,17 @@
+for(i = 0; i<=200; i++){
+    if (i === 0){
+        console.log(i+ "is even" );
+    }
+else if (i % 2 === 0){
+    console.log(i + "is even ");
+
+    }
+    else{
+        console.log(i + "is odd");
+    }
+}
+
+
+for(i=0; i<200; i=i+2){
+    console.log(i)
+}
