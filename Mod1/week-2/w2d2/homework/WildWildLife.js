16a4120b48981d987a020c88f532d8cc6f095ce6
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-06-06 23:32:12 -0400

    Wild Wild Life answered

---
+++ Mod1/week-2/w2d2/homework/WildWildLife.js
@@ -0,0 +1,13 @@
+const Wolfy = ["Wolfy", "Wolf", 16, "Yukon Territory"];
+const Sharky = ["Sharky", "Shark", 20, "Left Cost"]
+const Plantee = ["Plantee", "Plant", 5000, "Mordor"]
+const Porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const Dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+Plantee[2] += 1
+Wolfy[3] = "Gotham City"
+Dart.push("Hawkins")
+Wolfy.splice(0,1, "Gameboy");
+console.log(Plantee)
+console.log(Wolfy)
+console.log(Dart)
+console.log(Wolfy)
