d6f41d538b6332d4efcdad585fb091d66049c104
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-06-09 19:33:38 -0400

    Methods_revisited.js completed

---
+++ Mod1/week-2/w2d2/homework/Methods_revisited.js
@@ -0,0 +1,26 @@
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'How1\'s Moving Castle', 'Django unchained', 'Cloud Atlas', 
+'The usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 
+'Seven', 'Black Panther', 'Harry Potter', 'Limitation of life', 'Snatch', 'Fast and Furious'];
+
+
+
+const TheFirstHalf = console.log(favMovies.slice(8));
+favMovies.splice(3,1,  'Avator');
+favMovies.unshift();
+favMovies.shift();
+favMovies.reverse();
+favMovies.sort();
+favMovies.pop();
+console.log(favMovies);
+console.log(TheFirstHalf)
+console.log(favMovies);
+console.log(favMovies);
+console.log(favMovies);
+console.log(favMovies);
+console.log(favMovies);
+
+
+
+
+
+
