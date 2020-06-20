a811a6131a4482cdb380a99aab8784e2a838e686
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-06-20 03:52:18 -0400

    datatype refresher complete

---
+++ Mod1/week-3 /w3d3/homework/answers.js
@@ -0,0 +1,50 @@
+    //A light switch that can be either on or off
+    //Data type: boolean 
+    //example: const lightswitch = true;
+
+    //A user's email address
+    //Data type: String 
+    //Example: const users Email address = ""
+
+    //A spaceship with a hull,laser blasters,tractor beam, and warp drive
+    //Data type: array 
+    // const spaceship = ['hull', 'laser blasters', 'tractor beam', 'wrap drive'];
+
+    //A list of student names from our class
+    //Data type: array
+    // const student names = ['Mike', 'David', 'John'];
+
+    //A list of students names from our class each with a location
+    // Data type": Object
+    //const classmate names = [
+   // {
+   //    firstname: 'John',
+   //    location: 'California'
+   //  }
+   //  {
+   //    firstname: 'Mike',
+   //    location: 'Newyork'
+   //  }
+   //  ];
+   //
+   //
+   // A student names from out class each witha location and each with a list of favorite tv shows 
+   // Data type: object 
+   // 
+   // const studentnames = {
+   //    classmate1:[
+   //        person1:{
+   //            name: "john",
+   //            FavTvShows: 'Everybody loves raymond'
+   //        };
+//           person2:{
+   //            name:"Mike"'  
+   //            FavTvShows: "Criminal minds",
+   //       },
+   //       Person3: {
+   //           name: "David"
+   //            FavTvShows: 'Batman'
+   //       };
+   //  }
+
+
