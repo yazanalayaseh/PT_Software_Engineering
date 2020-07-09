522b271924c6b251cca1f03382ea7f84c2af63de
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-07-08 23:35:09 -0400

    Added more elements. My power grows with my dedication and hard work

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -2,6 +2,12 @@ $()) => {
 const $container = $('container');
 const $h1 = $("<h1/>").text("Hogwarts");
 $container.append($h1);
+const $h2 = $("<h2/>").text("Yazan");
+$container.append($h2);
+const $h3 = $("<h3/>").text("Hufflepuff, Gryffindor, Ravenclaw, or slytherin");
+$container.append($h3);
+const $h4animal = $("<h4/ class = "animal>").text("Shadow");
+$container.append($h4animal);
 });
 console.log($container);
 console.log($h1);
