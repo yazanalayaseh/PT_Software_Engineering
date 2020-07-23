be57c4d180d3cff9001171619d135dd6983ca583
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-07-23 00:58:03 -0400

    I relized I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -10,3 +10,26 @@ $container.append($h4animal);
 const $ul = $("<ul storage="trunk">").append("<li>Butter Beer"</li><li class="secret">invisibility cloak</li><li class ="secret">Marauders map</li><li class ="secret">time turner</li><li class ="animal">leash</li><li>Bertie Botts Every Flavor[jelly<Beans/li>");
 $$container.append($ul);
 
+const $h5 =$("<h5/>").text("spring 2017")
+$$container.append($h5);
+const $table = $("</table">);
+$container.append($table);
+const $thead =$("</$thead">);
+$container.append($thead);
+const $th1= $("<th/>").text("Day");
+$container.append($th1);
+const $th2 = $("<th/").text("Classes");
+$container.append($th2);
+
+
+const $trMonday= $("tr/>").append("<td>Monday</td><td>Herbology, Divination, History of Magic, Charms, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice, etc</td>");
+$container.append($trMonday);
+const $trTuesday= $("tr/>").append("<td>Tuesday</td><td>Herbology, Divination, History of Magic, Charms, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice, etc</td>");
+$container.append($trTuesday);
+const $trWednesday= $("tr/>").append("<td>Wednesday</td><td>Herbology, Divination, History of Magic, Charms, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice, etc</td>");
+$container.append($trWednesday;
+const $trThursday= $("tr/>").append("<td>Thursday</td><td>Herbology, Divination, History of Magic, Charms, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice, etc</td>");
+$container.append($trThursday);
+const $trFriday= $("tr/>").append("<td>Friday</td><td>Herbology, Divination, History of Magic, Charms, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice, etc</td>");
+$container.append($trFriday);
+
