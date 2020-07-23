7f81e7d54878515492f32642accfc2275626af44
PSCBUS_1016 <pscbus_1016@PSCBUS-1016.attlocal.net>
2020-07-22 23:35:03 -0400

    I aded list elements. That code didn't seem very dry.I wonder if there is something I could do about it in the hungry for More section

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -1,17 +1,12 @@
-$()) => {
-const $container = $('container');
-const $h1 = $("<h1/>").text("Hogwarts");
+const $container = $('#container');
+const $h1 = $("<h1>").text("Hogwarts");
 $container.append($h1);
 const $h2 = $("<h2/>").text("Yazan");
 $container.append($h2);
 const $h3 = $("<h3/>").text("Hufflepuff, Gryffindor, Ravenclaw, or slytherin");
 $container.append($h3);
-const $h4animal = $("<h4/ class = "animal>").text("Shadow");
+const $h4animal= $("<h4 class = "animal>").text("Shadow");
 $container.append($h4animal);
-});
-console.log($container);
-console.log($h1);
-
-
-
+const $ul = $("<ul storage="trunk">").append("<li>Butter Beer"</li><li class="secret">invisibility cloak</li><li class ="secret">Marauders map</li><li class ="secret">time turner</li><li class ="animal">leash</li><li>Bertie Botts Every Flavor[jelly<Beans/li>");
+$$container.append($ul);
 
